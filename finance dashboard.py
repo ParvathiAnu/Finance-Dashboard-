76dd43875f8b54d00e51b094124fffeabb547056
Nagulapalli Anu vinaya lakshmi parvathi <113771607+ParvathiAnu@users.noreply.github.com>
2024-06-14 15:14:25 +0530

    finance dashboard.py

--- finance dashboard.py
+++ finance dashboard.py
@@ -1,4 +1,3 @@
-import math
 import datetime as dt
 import numpy as np
 import pandas as pd
@@ -12,16 +11,20 @@ API_KEY = 'TT992S59LCHVQDCT'
 
 def load_data(ticker1, ticker2, start, end):
     ts = TimeSeries(key=API_KEY, output_format='pandas')
-    
-    df1, _ = ts.get_daily_adjusted(symbol=ticker1, outputsize='full')
-    df2, _ = ts.get_daily_adjusted(symbol=ticker2, outputsize='full')
-    
+
+    try:
+        df1, _ = ts.get_daily_adjusted(symbol=ticker1, outputsize='full')
+        df2, _ = ts.get_daily_adjusted(symbol=ticker2, outputsize='full')
+    except Exception as e:
+        print(f"Error fetching data: {e}")
+        return None, None
+
     df1 = df1.loc[start:end]
     df2 = df2.loc[start:end]
-    
+
     df1.columns = [col.split('. ')[1] for col in df1.columns]
     df2.columns = [col.split('. ')[1] for col in df2.columns]
-    
+
     df1.index = pd.to_datetime(df1.index)
     df2.index = pd.to_datetime(df2.index)
 
@@ -38,7 +41,7 @@ def update_plot(data, indicators, sync_axis=None):
     else:
         p = figure(x_axis_type="datetime", tools="pan,wheel_zoom,box_zoom,reset,save", width=1000)
 
-    p.xaxis.major_label_orientation = math.pi / 4
+    p.xaxis.major_label_orientation = np.pi / 4
     p.grid.grid_line_alpha = 0.3
 
     p.segment(df.index, df['high'], df.index, df['low'], color="black")
@@ -57,8 +60,7 @@ def update_plot(data, indicators, sync_axis=None):
             slope = par[0][0]
             intercept = par[0][1]
             y_predicted = [slope * i + intercept for i in range(len(df.index.values))]
-            p.segment(df.index[0], y_predicted[0], df.index[-1], y_predicted[-1], legend_label="Linear Regression",
-                      color="red")
+            p.line(df.index, y_predicted, color="red", legend_label="Linear Regression Line")
 
     p.legend.location = "top_left"
     p.legend.click_policy = "hide"
@@ -66,21 +68,26 @@ def update_plot(data, indicators, sync_axis=None):
     return p
 
 def on_button_click(main_stock, comparison_stock, start, end, indicators):
-    source1, source2 = load_data(main_stock, comparison_stock, start, end)
-    p = update_plot(source1, indicators)
-    p2 = update_plot(source2, indicators, sync_axis=p.x_range)
-    curdoc().clear()
-    curdoc().add_root(layout)
-    curdoc().add_root(row(p, p2))
+    start_date = pd.to_datetime(start)
+    end_date = pd.to_datetime(end)
+    source1, source2 = load_data(main_stock, comparison_stock, start_date, end_date)
+    
+    if source1 is not None and source2 is not None:
+        p = update_plot(source1, indicators)
+        p2 = update_plot(source2, indicators, sync_axis=p.x_range)
+        
+        curdoc().clear()
+        curdoc().add_root(layout)
+        curdoc().add_root(row(p, p2))
 
 stock1_text = TextInput(title="Main Stock")
 stock2_text = TextInput(title="Comparison Stock")
-date_picker_from = DatePicker(title='Start Date', value="2020-01-01", min_date="2000-01-01", max_date=dt.datetime.now().strftime("%Y-%m-%d"))
-date_picker_to = DatePicker(title='End Date', value="2020-02-01", min_date="2000-01-01", max_date=dt.datetime.now().strftime("%Y-%m-%d"))
+date_picker_from = DatePicker(title='Start Date', value=dt.date(2020, 1, 1), min_date=dt.date(2000, 1, 1), max_date=dt.date.today())
+date_picker_to = DatePicker(title='End Date', value=dt.date(2020, 2, 1), min_date=dt.date(2000, 1, 1), max_date=dt.date.today())
 indicator_choice = MultiChoice(options=["100 Day SMA", "30 Day SMA", "Linear Regression Line"])
 
 load_button = Button(label="Load Data", button_type="success")
-load_button.on_click(lambda: on_button_click(stock1_text.value, stock2_text.value, date_picker_from.value, date_picker_to.value, indicator_choice.value))
+load_button.on_click(lambda event: on_button_click(stock1_text.value, stock2_text.value, date_picker_from.value, date_picker_to.value, indicator_choice.value))
 
 layout = column(stock1_text, stock2_text, date_picker_from, date_picker_to, indicator_choice, load_button)
 
